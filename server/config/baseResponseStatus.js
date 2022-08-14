ea237a2d459c0a2ad1f207bae721a1816a4bb955
wendy <23jiwon@gmail.com>
2022-08-15 01:07:49 +0900

    C:/Program Files/Git/api 추가

--- server/config/baseResponseStatus.js
+++ server/config/baseResponseStatus.js
@@ -51,7 +51,7 @@ module.exports = {
     SIGNIN_EMAIL_WRONG : { "isSuccess": 0, "code": 3003, "message": "이메일이 잘못 되었습니다." },
     SIGNIN_PASSWORD_WRONG : { "isSuccess": 0, "code": 3004, "message": "비밀번호가 잘못 되었습니다." },
     SIGNIN_INACTIVE_ACCOUNT : { "isSuccess": 0, "code": 3005, "message": "비활성화 된 계정입니다. 고객센터에 문의해주세요." },
-    SIGNIN_WITHDRAWAL_ACCOUNT : { "isSuccess": 0, "code": 3006, "message": "탈퇴 된 계정입니다. 고객센터에 문의해주세요." },
+    SIGNIN_WITHDRAWAL_ACCOUNT : { "isSuccess": 0, "code": 3006, "message": "탈퇴된 계정입니다. 고객센터에 문의해주세요." },
 
     //Connection, Transaction 등의 서버 오류
     DB_ERROR : { "isSuccess": 0, "code": 4000, "message": "데이터 베이스 에러"},
