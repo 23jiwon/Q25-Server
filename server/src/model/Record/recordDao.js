3b1b87c8c43813a844dd45afb7e451fa73c62259
wendy <23jiwon@gmail.com>
2022-08-18 14:28:00 +0900

    답변 모아보기 수정중

--- server/src/model/Record/recordDao.js
+++ server/src/model/Record/recordDao.js
@@ -174,6 +174,7 @@ async function SelectCollection(connection, userIdx) {
         selectQuestion,
         userIdx
     );
+
     //답변정보
     const selectAnswer = `
     SELECT questionIdx as qNum, answer
@@ -189,6 +190,7 @@ async function SelectCollection(connection, userIdx) {
     let newQ = []
     for(let i = 0 ; i < selectAnswerRow.length ; i++){
         let num = selectAnswerRow[i].qNum
+        console.log("num:", num);
         let t = {qnacontent : selectQuestionRow[num].qnacontent}
         newQ.push(t);
     }
@@ -252,11 +254,12 @@ async function SelectQlist(connection, userIdx) {
         selectQuestion,
         userIdx
     );
+
     //답변정보
     const selectAnswer = `
     SELECT questionIdx as qNum, opened, answer
     FROM christmas25.pagetbl
-    WHERE userIdx = ? 
+    WHERE userIdx = ?
     `;
     const [selectAnswerRow] = await connection.query(
         selectAnswer,
@@ -277,10 +280,10 @@ async function SelectQlist(connection, userIdx) {
 
     //이미지 정보
     const selectImg = `
-    SELECT  CONCAT('http://localhost:5000/christmasQ25_asset/', imageUrl) as stampImg
+    SELECT  CONCAT('http://localhost:5000/christmasQ25_asset', imageUrl) as stampImg 
     FROM christmas25.imagetbl
     WHERE imageIdx = 2
-    `;
+    `;//imageUrl에 /가 포함되어 있어 asset뒤의 / 지웠습니다
     const [selectImgRow] = await connection.query(
         selectImg
     );
--- server/src/model/User/userService.js
+++ server/src/model/User/userService.js
@@ -141,11 +141,11 @@ exports.sendPw = async function (userEmail) {
     const connection = await pool.getConnection(async (conn) => conn);
     try{
         let mailOptions = {
-            from : process.env.EMAIL_USER, //TODO : 팀 이름 결정되면 수정
+            from : process.env.EMAIL_USER,
             to : userEmail,
-            subject : '[TEST] ChristmasQ25에서 임시 비밀번호를 알려드립니다',//TODO : 팀 이름 결정되면 수정
+            subject : 'Rudolf에서 임시 비밀번호를 알려드립니다',//TODO : 팀 이름 결정되면 수정
             html : `
-            <h1>ChristmasQ25에서 임시 비밀번호를 알려드립니다.</h1><br>
+            <h1>Rudolf에서 임시 비밀번호를 알려드립니다.</h1><br>
             <h3> 임시 비밀번호 : `+randomPassword+`</h3>
             <br><h3>임시 비밀번호로 로그인 하신 후, 반드시 비밀번호를 수정해 주세요.</h3>
             `
