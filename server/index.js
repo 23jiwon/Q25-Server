14066301e2f5a806e7affacdcc0f8adb7384dbad
wendy <23jiwon@gmail.com>
2022-08-21 19:29:10 +0900

    axios error해결해야함

--- server/index.js
+++ server/index.js
@@ -7,4 +7,4 @@ const port = process.env.port;
 express().listen(port);
 logger.info(`${process.env.NODE_ENV } - API Server Start At port ${port}`);
 
-recordService.updateOpened();
+// recordService.updateOpened();
